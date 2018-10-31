e28da069d238eb85de794c93c65761a5fb302b1a
gabrielvv <gabrielvautrin@gmail.com>
2018-10-31 12:14:50 +0100

    authActions remplace authService

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,26 +1,41 @@
 /**
  * App Component
  */
-import { Component } from '@angular/core'
+import { Component, OnDestroy } from '@angular/core'
 import { select } from "@angular-redux/store";
 import { Router } from "@angular/router";
 import * as queries from './store/auth/auth.queries'
-import { Observable } from 'rxjs';
+import { Observable, Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.html'
 })
-export class AppComponent {
-  @select(queries.isOpened) opened : Observable<boolean>
+export class AppComponent implements OnDestroy {
+  
+  subscriptions : Subscription[] = []
+  @select(queries.isOpened) isLoginPageOpened : Observable<boolean>
+  @select(queries.isAuthenticated) isAuthenticated : Observable<boolean>
+
+  ngOnDestroy(): void {
+    this.subscriptions.forEach(Subscription.prototype.unsubscribe.call)
+  }
 
   constructor(private router : Router){
-    this.opened.subscribe(b => {
+    // MAP STATE AND AND ROUTING
+
+    this.subscriptions.push(this.isAuthenticated.subscribe(b => {
+      if(!b){
+        this.router.navigate(['']);
+      }
+    }))
+
+    this.subscriptions.push(this.isLoginPageOpened.subscribe(b => {
         if(!b) {
           this.router.navigate(['']);
         } else {
           this.router.navigate(['login']);
         }
-    })
+    }))
   }
 }
--- src/app/services/auth/auth.guard.ts
+++ src/app/services/auth/auth.guard.ts
@@ -3,48 +3,57 @@
  *
  * - connected ?
  */
-import { Injectable } from '@angular/core';
+import { Injectable, OnDestroy } from '@angular/core';
 import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot, Router } from '@angular/router';
-import { AuthService } from './auth.service';
 import { tap, take } from 'rxjs/operators';
 import { AuthActions } from 'src/app/store/auth/auth.actions';
+import { isAuthenticated } from 'src/app/store/auth/auth.queries';
+import { Observable, Subscription } from 'rxjs';
+import { select } from '@angular-redux/store';
 
 @Injectable()
-export class AuthGuard implements CanActivate {
+export class AuthGuard implements CanActivate, OnDestroy {
+  ngOnDestroy(): void {
+    this.subscriptions.forEach(Subscription.prototype.unsubscribe.call)
+  }
 
+  @select(isAuthenticated) isAuthenticated : Observable<boolean>
+  subscriptions : Subscription[] = []
   pendingPath : string
 
   constructor(
-    private authService: AuthService, 
     private authActions: AuthActions,
     private router: Router
-  ) {}
+  ) {
+      const sub = this.isAuthenticated
+        .subscribe(authenticated => {
+          if(!authenticated || !this.pendingPath) return
+          this.redirectToPendingPath() 
+        })
+      
+      this.subscriptions.push(sub)
+  }
+
+  redirectToPendingPath(){
+    const path = this.pendingPath
+    this.pendingPath = undefined
+    this.authActions.close()
+    path && this.router.navigate([path])
+  }
 
   canActivate(
     route: ActivatedRouteSnapshot,
     state: RouterStateSnapshot
   ) {
-
-    console.log(route.url[0].path)
-
-    return this.authService.isAuthenticated()
-    .pipe(
-        tap(authenticated => console.log('authenticated=', authenticated)),
-        tap(authenticated => {
-          if (!authenticated) {
-            this.pendingPath = route.url[0].path
-            this.authActions.open()
-            this.authService.isAuthenticated()
-              .pipe(take(2)).subscribe(isAuthenticated => {
-                if(!isAuthenticated) return
-                
-                const path = this.pendingPath
-                this.pendingPath = undefined
-                this.authActions.close()
-                path && this.router.navigate([path])
-              })
-          }
-        }),
+    return this.isAuthenticated
+      .pipe(
+          tap(authenticated => console.log('authenticated=', authenticated)),
+          tap(authenticated => {
+            if (!authenticated) {
+              this.pendingPath = route.url[0].path
+              this.authActions.open();
+            }
+          }),
       );
   }
 }
